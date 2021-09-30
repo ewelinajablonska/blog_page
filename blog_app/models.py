b768ac097d78dae5dd5b5e31351bf61893c27690
Ewelina <ewelina.jablonska.pl@gmail.com>
2021-09-30 12:57:19 +0200

    create a first model for post

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.fields import TextField
 
 # Create your models here.
+class BlogPost(models.Model):
+    """Create a new post"""
+    title = models.TextField(max_length=70)
+    text = models.TextField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.title
