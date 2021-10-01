3fb6118dd1bc781831e7ad1f48a5355f25e28cf5
Ewelina <ewelina.jablonska.pl@gmail.com>
2021-10-01 12:23:03 +0200

    add posts on the home page

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from blog_app.models import BlogPost
+
 # Create your views here.
 def index(request):
     """The home page for blog application."""
-    return render(request, 'blog_app/index.html')
+    posts = BlogPost.objects.order_by('date_added')
+    context = { 'posts' : posts}
+    return render(request, 'blog_app/index.html', context)
