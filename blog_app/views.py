e969d76f5b42275d342b848a503b5cefb702bde5
Ewelina <ewelina.jablonska.pl@gmail.com>
2021-10-01 11:40:01 +0200

    create view for home page

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """The home page for blog application."""
+    return render(request, 'blog_app/index.html')
