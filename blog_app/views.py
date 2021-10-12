05d4cdfa6ba56c1a63886494c8fc1189c742d5ac
Ewelina <ewelina.jablonska.pl@gmail.com>
2021-10-12 22:06:08 +0200

    restrict datas acces to user

--- blog_app/views.py
+++ blog_app/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     """The home page for blog application."""
-    posts = BlogPost.objects.order_by('date_added')
+    posts = BlogPost.objects.filter(owner=request.user).order_by('date_added')
     context = { 'posts' : posts}
     return render(request, 'blog_app/index.html', context)
 
